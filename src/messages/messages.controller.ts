b8a3c94585ccd758fc32953ce7ef41b1de923ab5
eddymandran <eddy.mandran@gmail.Com>
2022-09-13 11:43:45 +0200

    Manual testing of the controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,30 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { CreateMessageDto } from "./dtos/create-message.dto";
+import { MessagesService } from "./messages.service";
 
 @Controller("messages")
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DON'T DO THIS ON REAL APPS
+    // USE DEPENDENCY INJECTION INSTEAD
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get("/:id")
-  getMessages(@Param('id') id: string ) {
-  console.log(id)
-}
+  getMessages(@Param("id") id: string) {
+    return this.messagesService.findOne(id);
+  }
 
 }
